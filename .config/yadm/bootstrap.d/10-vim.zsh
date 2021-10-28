701299887be6ba735f243c8c0dc3aa641a5af846
Augusto Elesbão <aelesbao@gmail.com>
2021-10-29 00:35:59 +0200

    bootstrap vim

---
+++ .config/yadm/bootstrap.d/10-vim.zsh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+#
+# Vim setup
+#
+
+set -euo pipefail
+
+if ! (( $+commands[vim] )); then
+  msg "vim not installed"
+  return 0
+fi
+
+info "Configuring Vim"
+
+if ! [[ -f "${HOME}/.vimrc" ]]; then
+  msg "~/.vimrc not found"
+  return 1
+fi
+
+if [[ -d "${HOME}/.vim/bundle" ]]; then
+  if ! ask "It seems like vim is already configured. Do you want to proceed anyway?"; then
+    return 0
+  fi
+fi
+
+mkdir -p ~/.vim
+
+[ $# -eq 0 ] && set -- -
+vim +'BundleInstall' +'BundleClean!' +'BundleUpdate' +'qall' -R
