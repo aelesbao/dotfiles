8c94af66052b259568c64844b3215eda92ec6697
Augusto Elesbão <aelesbao@gmail.com>
2021-10-30 00:07:13 +0200

    bootstrap rust

---
+++ .config/yadm/bootstrap.d/10-rust.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+#
+# Rust setup
+#
+
+set -euo pipefail
+
+if ! (( $+commands[rustup] )); then
+  msg "rustup not installed"
+  return 0
+fi
+
+info "Configuring Rust"
+
+rustup update nightly
+rustup update stable
+
+rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup component add rustfmt
