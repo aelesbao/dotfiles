037ff5310f62f974ccab298b56564d77fbd76bf4
Augusto Elesbão <aelesbao@gmail.com>
2021-11-01 01:36:17 +0100

    check if user signed in into the App Store

--- .config/yadm/bootstrap.d/03-homebrew.zsh
+++ .config/yadm/bootstrap.d/03-homebrew.zsh
@@ -10,6 +10,18 @@ if ! is-macos; then
   return 0
 fi
 
+require mas
+
+if ! mas account >/dev/null 2>&1; then
+  warn "Not logged in into Mac App Store"
+  if ask "Do you want to sign in into the App Store now?"; then
+    osascript -e '
+      tell application "App Store"
+        activate
+      end tell'
+  fi
+fi
+
 if [[ -f "$HOME/.Brewfile" ]]; then
   info "Updating Homebrew bundle"
   brew bundle --global --cleanup
