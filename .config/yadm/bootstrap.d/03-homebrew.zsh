f1eca139dddf989d70f8b55ba8e96bff15ed3e7e
Augusto Elesbão <aelesbao@gmail.com>
2021-11-01 01:47:41 +0100

    better messages on waiting for sign in

--- .config/yadm/bootstrap.d/03-homebrew.zsh
+++ .config/yadm/bootstrap.d/03-homebrew.zsh
@@ -13,12 +13,19 @@ fi
 require mas
 
 if ! mas account >/dev/null 2>&1; then
-  warn "Not logged in into Mac App Store"
-  if ask "Do you want to sign in into the App Store now?"; then
+  warn "To install all packages from the Homebrew Bundle file, you should sign in into the Mac App Store"
+  if ask "Do you want to do it now?"; then
     osascript -e '
       tell application "App Store"
         activate
       end tell'
+
+    msg "Waiting for the user to sign in..."
+    until mas account >/dev/null 2>&1; do
+      sleep 3
+    done
+
+    notice "Done!"
   fi
 fi
 
