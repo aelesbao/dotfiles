79e73f802f8e183da5429aa635e9e6b5f9ef31c1
Augusto Elesbão <aelesbao@gmail.com>
2021-11-28 18:05:04 +0100

    bootstrap script for Node.js

---
+++ .config/yadm/bootstrap.d/04-nodejs.zsh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+#
+# Node.js setup
+#
+
+set -euo pipefail
+
+if ! (( $+commands[nvm] )); then
+  if [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
+    source "/usr/local/opt/nvm/nvm.sh"
+  else
+    msg "nvm not found, skipping..."
+    return 1
+  fi
+fi
+
+info "Configuring Node.js"
+
+mkdir ~/.nvm
+export NVM_DIR="$HOME/.nvm"
+
+nvm install --lts
+nvm install node --default
