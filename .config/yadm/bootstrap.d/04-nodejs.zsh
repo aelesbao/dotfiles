98fa7b9f9dd4edeebf543fd5a4dfccaed5e30299
Augusto Elesbão <aelesbao@gmail.com>
2021-12-06 22:28:18 +0100

    load nvm from home folder

--- .config/yadm/bootstrap.d/04-nodejs.zsh
+++ .config/yadm/bootstrap.d/04-nodejs.zsh
@@ -8,6 +8,8 @@ set -euo pipefail
 if ! (( $+commands[nvm] )); then
   if [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
     source "/usr/local/opt/nvm/nvm.sh"
+  elif [ -s "$HOME/.nvm/nvm.sh" ]; then
+    source "$HOME/.nvm/nvm.sh"
   else
     msg "nvm not found, skipping..."
     return 1
