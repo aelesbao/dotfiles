ed071ff5f45e26600c07514071704265c8e83005
Augusto Elesbão <aelesbao@gmail.com>
2021-12-06 22:28:34 +0100

    install base packages for Ubuntu

---
+++ .config/yadm/bootstrap.d/00-ubuntu.zsh
@@ -0,0 +1,30 @@
+#!/usr/bin/env zsh
+#
+# Ubuntu settings
+#
+
+set -euo pipefail
+
+if ! is-ubuntu; then
+  msg "Not in a Ubuntu"
+  return
+fi
+
+
+info "Update and upgrade packages"
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y \
+  build-essential \
+  curl \
+  file \
+  finger \
+  language-pack-en-base \
+  powerline \
+  procps \
+  rsync \
+  vim \
+  wget \
+  zip
