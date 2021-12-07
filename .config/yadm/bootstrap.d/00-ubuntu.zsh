90bad38e1c83a6b833904498209cd9d3656a6218
Augusto Elesbão <aelesbao@gmail.com>
2021-12-07 13:54:19 +0100

    install nerd fonts on Ubuntu

--- .config/yadm/bootstrap.d/00-ubuntu.zsh
+++ .config/yadm/bootstrap.d/00-ubuntu.zsh
@@ -5,6 +5,30 @@
 
 set -euo pipefail
 
+function download-gh-release() {
+  set -euo pipefail
+
+  local repo="${1}"
+  local file="${2}"
+  local version="${3:-$(get-latest-tag "${repo}")}"
+  local pkg_path="$(mktemp -d)/${file}"
+
+  curl -fsSLo "${pkg_path}" "https://github.com/${repo}/releases/download/${version}/${file}"
+  echo "${pkg_path}"
+}
+
+function install-gh-pkg-release() {
+  set -euo pipefail
+
+  local repo="${1}"
+  local pkg_name="${2:-${repo#*/}}"
+  local version="${3:-$(get-latest-tag "${repo}")}"
+  local pkg_path="$(download-gh-release "${repo}" "${pkg_name}_${version}_amd64.deb" "${version}")"
+
+  msg "Installing ${repo}"
+  sudo dpkg -i "${pkg_path}"
+}
+
 if ! is-ubuntu; then
   msg "Not in a Ubuntu"
   return
@@ -15,7 +39,7 @@ info "Update and upgrade packages"
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update && sudo apt upgrade -y
 
-info "Installing new packages"
+info "Installing apt packages"
 sudo apt install -y \
   build-essential \
   curl \
@@ -31,9 +55,12 @@ sudo apt install -y \
   zip
 
 info "Installing manual packages"
+install-gh-pkg-release "Peltoche/lsd"
 
-msg "lsd"
-version="$(get-latest-tag Peltoche/lsd)"
-pkg_path="$(mktemp -d)/lsd_${version}_amd64.deb"
-wget -O "${pkg_path}" "https://github.com/Peltoche/lsd/releases/download/${version}/lsd_${version}_amd64.deb"
-sudo dpkg -i "${pkg_path}"
+info "Installing fonts"
+mkdir -p ~/.local/share/fonts
+unzip -u "$(download-gh-release ryanoasis/nerd-fonts Hack.zip)" -d ~/.local/share/fonts
+
+info "Cleaning up"
+sudo apt autoremove -y
+sudo apt autoclean
