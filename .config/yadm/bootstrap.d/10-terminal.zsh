58fab8045a5e0840754327ecaa00fe73ea8abcfa
Augusto Elesbão <aelesbao@gmail.com>
2022-02-10 13:43:28 +0100

    update Kitty config

--- .config/yadm/bootstrap.d/10-terminal.zsh
+++ .config/yadm/bootstrap.d/10-terminal.zsh
@@ -15,3 +15,24 @@ if is-macos && is-pkg-installed iterm2; then
     curl -fsSL https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
   fi
 fi
+
+if has-command kitty; then
+  info "Configuring Kitty"
+
+  msg "Set theme"
+  kitty +kitten themes --reload-in all --dump-theme Jellybeans >! ~/.config/kitty/current-theme.conf
+
+  if is-macos; then
+    kitty_app_path=$(find /Applications -name "kitty.app" -maxdepth 1)
+
+    msg "Set icon"
+    cp ~/.config/kitty/kitty-icon/kitty.icns ${kitty_app_path}/Contents/Resources/kitty.icns
+    cp ~/.config/kitty/kitty-icon/icon_128x128.png ${kitty_app_path}/Contents/Resources/kitty/logo/kitty-128.png
+    cp ~/.config/kitty/kitty-icon/icon_256x256.png ${kitty_app_path}/Contents/Resources/kitty/logo/kitty.png
+
+    msg "Refresh icon cache"
+    touch ${kitty_app_path}
+    rm /var/folders/*/*/*/com.apple.dock.iconcache
+    killall Dock
+  fi
+fi
