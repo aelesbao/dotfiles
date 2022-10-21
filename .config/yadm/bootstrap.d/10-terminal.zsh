2821e15611c6337a1712eceeb9d4f72adcbcdae2
Augusto Elesbão <aelesbao@gmail.com>
2022-10-21 20:42:01 +0200

    fix(iTerm2): set correct path to load the config

--- .config/yadm/bootstrap.d/10-terminal.zsh
+++ .config/yadm/bootstrap.d/10-terminal.zsh
@@ -9,7 +9,7 @@ if is-macos && is-pkg-installed iterm2; then
   info "Configuring iTerm"
 
   # Configure shared settings folder
-  defaults write com.googlecode.iterm2 PrefsCustomFolder "~/Dropbox/Apps/iTerm"
+  defaults write com.googlecode.iterm2 PrefsCustomFolder "~/.config/iterm2"
 
   if ! [[ -f ~/.iterm2_shell_integration.zsh ]]; then
     curl -fsSL https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
