3faefa3601599e9fa253c55a3e6e48234d8a025d
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-06-20 17:42:10 +0200

    feat: add gh-cli extensions

---
+++ .config/yadm/bootstrap.d/10-github.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+#
+# GitHub settings
+#
+
+set -euo pipefail
+
+require gh
+
+info "Configuring gh-cli"
+extensions=(
+  nektos/gh-act
+  dlvhdr/gh-dash
+  meiji163/gh-notify
+)
+for ext in $extensions; do
+  if gh ext list | grep $ext > /dev/null; then
+    gh ext upgrade $ext
+  else
+    gh ext install $ext
+  fi
+done
