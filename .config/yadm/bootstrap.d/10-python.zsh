b35ec6970d95297da567f631662ecb5ccce1bfb0
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-11-20 15:16:11 +0100

    feat(yadm): add Python bootstrap script

---
+++ .config/yadm/bootstrap.d/10-python.zsh
@@ -0,0 +1,44 @@
+#!/usr/bin/env zsh
+#
+# Python setup
+#
+
+set -euo pipefail
+
+if ! (( $+commands[pyenv] )); then
+  info "Installing pyenv"
+  if is-macos; then
+    brew install pyenv
+  else
+    curl https://pyenv.run | bash
+  fi
+fi
+
+python_version="$(pyenv install -l | egrep '^  ([0-9]+\.[0-9]+\.[0-9]+)$' | tail -1 | sed 's/ //g')"
+info "Configuring Python $python_version"
+
+if ! pyenv versions | grep -s "$python_version" >/dev/null; then
+  info "Installing Python $python_version"
+  pyenv install "$python_version"
+fi
+
+msg "Using $python_version"
+pyenv shell "$python_version"
+
+python3 -m pip install --upgrade pip virtualenv
+
+if ! (( $+commands[pipx] )); then
+  info "Installing pipx"
+  if is-macos; then
+    brew install pipx
+    pipx ensurepath
+  else
+    python3 -m pip install --user pipx
+    python3 -m pipx ensurepath
+  fi
+fi
+
+info "Installing pipx packages"
+pipx install poetry
+pipx install --include-deps ansible
+pipx inject ansible ansible-lint
