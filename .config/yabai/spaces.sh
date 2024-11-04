f2b19398f97a5ba7e552ebe863a7f634ab5dc449
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-11-04 10:58:21 +0100

    refactor(yabai): re-organize spaces

--- .config/yabai/spaces.sh
+++ .config/yabai/spaces.sh
@@ -4,6 +4,9 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 . "$SCRIPT_DIR/util.sh"
 
+# idx_email_space="$(yabai query --windows | jq -r '.[] | select(.app == "Mimestream") | .space')"
+# idx_slack_space="$(yabai query --windows | jq -r '.[] | select(.app == "Slack") | .space')"
+
 # display 1
 yabai -m space 1 --label email
 yabai -m space 2 --label slack
@@ -15,22 +18,15 @@ if has_display "$samsung_odyssey_full"; then
     echo "Configuring spaces with external monitor"
 
     yabai -m space 5 --label planning
-    yabai -m space 6 --label philabs-browsing
-    yabai -m space 7 --label philabs-work
-    yabai -m space 8 --label dotfiles
-    yabai -m space 9 --label home-lab
-    yabai -m space 10 --label research
+    yabai -m space 6 --label browsing
+    yabai -m space 7 --label work
 
     yabai -m space planning --balance
-    yabai -m space research --balance
 else
     echo "Configuring spaces without external monitor"
 
-    yabai -m space 5 --label philabs-browsing
-    yabai -m space 6 --label philabs-work
-    yabai -m space 7 --label dotfiles
-    yabai -m space 8 --label home-lab
-    yabai -m space 9 --label research
+    yabai -m space 5 --label browsing
+    yabai -m space 6 --label work
 fi
 
 yabai -m config --space personal layout stack
