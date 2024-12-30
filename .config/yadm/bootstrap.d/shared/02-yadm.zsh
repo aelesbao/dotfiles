cba31de13d5884f7245404900baac9ac1279a973
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-12-30 23:40:15 +0100

    refactor(yadm): initialize yadm submodules earlier in the boot

--- .config/yadm/bootstrap.d/shared/02-yadm.zsh
+++ .config/yadm/bootstrap.d/shared/02-yadm.zsh
@@ -8,8 +8,5 @@ set -euo pipefail
 info "Updating the yadm repo origin URL"
 yadm remote set-url origin "git@github.com:$GITHUB_USER/dotfiles.git"
 
-info "Init submodules"
-yadm submodule update --recursive --init
-
 info "Decrypting secret files"
 yadm decrypt
