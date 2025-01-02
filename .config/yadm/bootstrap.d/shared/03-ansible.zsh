338e1fe98be8515b0344f0745ac51fb599f1be05
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-01-02 09:35:01 +0100

    feat(ansible): run playbook during bootstrap

--- .config/yadm/bootstrap.d/shared/03-ansible.zsh
+++ .config/yadm/bootstrap.d/shared/03-ansible.zsh
@@ -6,3 +6,18 @@
 set -euo pipefail
 
 require ansible
+
+info "Ansible setup"
+
+SCRIPT_PATH="${0:A:h}"
+ANSIBLE_ROOT="${SCRIPT_PATH}/../../ansible"
+
+pushd "$ANSIBLE_ROOT"
+
+msg "Installing Ansible collections and roles"
+ansible-galaxy install -r requirements.yml
+
+msg "Running Ansible playbook"
+ansible-playbook playbook.yml --ask-become-pass
+
+popd
