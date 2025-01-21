5249eb3fae1718a6cb1029a5d8afbb5b853e9cc0
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-01-21 15:19:14 +0100

    feat(yadm): authorize public ssh keys from GitHub

--- .config/yadm/bootstrap.d/shared/01-ssh.zsh
+++ .config/yadm/bootstrap.d/shared/01-ssh.zsh
@@ -5,6 +5,11 @@
 
 set -euo pipefail
 
+if [[ -z "${GITHUB_USER:-}" ]]; then
+  fail "GITHUB_USER is not set"
+fi
+
+
 function keygen() {
   local key_type="${1}"
   local key_file="${HOME}/.ssh/id_${key_type}"
@@ -16,28 +21,34 @@ function keygen() {
     ssh-keygen -t "$key_type" -C "$key_name" -f "$key_file" -q -N ""
     msg "SSH key $key_type created"
   fi
+
+  declare public_key="$(ssh-keygen -y -f "$key_file" | cut -d ' ' -f 1,2)"
+
+  if ! grep -q "^${public_key}" ~/.ssh/authorized_keys; then
+    cat "${key_file}.pub" >> ~/.ssh/authorized_keys
+    msg "Authorized $key_type public key"
+  fi
 }
 
-info "Configuring SSH"
 
-keygen rsa
+info "Creating SSH keys"
+
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
 keygen ed25519
 
-declare gh_key_type="ed25519"
-declare gh_key="${HOME}/.ssh/id_${gh_key_type}"
 
-declare public_key="$(ssh-keygen -y -f "$gh_key" | cut -d ' ' -f 1,2)"
-declare github_keys="$(curl -fsSL "https://api.github.com/users/${GITHUB_USER}/keys")"
-if [[ "$github_keys" == *"$public_key"* ]]; then
-  msg "SSH key ${gh_key_type} already added to GitHub"
-else
-  if ask "Do you want to add the ${gh_key_type} public key to your GitHub account?"; then
-    notice "Copy and paste the key content into GitHub:"
-    echo "$public_key"
-    open "https://github.com/settings/ssh/new"
-  fi
+info "Adding ${GITHUB_USER} GitHub public keys to ~/.ssh/authorized_keys"
 
-  if ! ask "Did you add the public key to your account?"; then
-    warn "If you have issues executing the scripts bellow, try adding the key and run it again"
+IFS=$'\n' # make newlines the only separator
+for key in $(curl -fsSL "https://github.com/${GITHUB_USER}.keys"); do
+  msg "$key"
+  if ! grep -q "^$key" ~/.ssh/authorized_keys; then
+    echo "$key ${GITHUB_USER}@users.noreply.github.com" >> ~/.ssh/authorized_keys
+    notice "added to authorized keys"
   fi
-fi
+done
+
+# reset IFS
+unset IFS
