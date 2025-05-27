513ac0386878c6078af7c40b74a8344fcf239a93
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-05-27 12:37:03 +0200

    feat(archlinux): import Chaotic AUR

--- .config/yadm/bootstrap.d/linux/arch.zsh
+++ .config/yadm/bootstrap.d/linux/arch.zsh
@@ -11,10 +11,26 @@ if ! has-command pacman; then
 fi
 
 
-if ! [[ -f /etc/pacman.d/mirrorlist.backup ]]; then
+info "Adding Chaotic AUR repository"
+
+msg "Import primary key"
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+msg "Install chaotic-mirrorlist and chaotic-keyring"
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+msg "Add repository to /etc/pacman.conf"
+if ! grep -q "^[chaotic-aur]$" /etc/pacman.conf; then
+  echo -e "\n\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf > /dev/null
+fi
+
+
+if ! [[ -f /etc/pacman.d/mirrorlist.bak ]]; then
   info "Optmize mirror list"
 
-  sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+  sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
   if has-command pacman-mirrors; then
     sudo pacman-mirrors \
@@ -34,12 +50,15 @@ if ! [[ -f /etc/pacman.d/mirrorlist.backup ]]; then
 fi
 
 
-info "Update and upgrade packages"
-sudo pacman -Syyu --noconfirm
+info "Sync packages"
+sudo pacman -Syyu
 
+info "Install base packages"
 add-pkg \
   base-devel \
   curl \
   git \
+  jq \
+  go-yq \
   python \
   python-jmespath
