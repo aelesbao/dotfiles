2de3e5d28167cacbf8611eb575d10e66346aa31d
Augusto Elesbão <aelesbao@users.noreply.github.com>
2026-01-20 00:00:23 +0100

    feat(voxtype): fix install script

--- .config/yadm/bootstrap.d/shared/10-voxtype.zsh
+++ .config/yadm/bootstrap.d/shared/10-voxtype.zsh
@@ -12,7 +12,6 @@ fi
 
 
 info "Configuring voxtype"
-voxtype setup
 voxtype setup --download
 
 if ask "Select model to run?"; then
@@ -23,9 +22,8 @@ if ask "Enable GPU support?"; then
   sudo voxtype setup gpu --enable
 fi
 
-msg "Checking setup status"
+info "Checking setup status"
 voxtype setup check
 
-msg "Setting up systemd service"
+info "Setting up systemd service"
 voxtype setup systemd
-systemctl --user enable --now voxtype
